abd47d4297f8190fe1541cdd07296a1d2cba1bc4
Adrian Jones Abache <90118579+ajabache@users.noreply.github.com>
2023-07-19 09:30:16 +0800

    Add files via upload

---
+++ heat_index_monitoring/heat_index.ino
@@ -0,0 +1,120 @@
+#include <dht.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+dht DHT;
+#define DHT11_PIN 7
+#define BUZZER_PIN 8
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  lcd.begin();// Initialize the LCD
+
+  lcd.backlight();   // Turn on the backlight
+}
+
+void loop() {
+  int chk = DHT.read11(DHT11_PIN);
+
+  if (DHT.temperature >= 27 && DHT.temperature <= 31 && DHT.humidity >=45) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("T=");
+    lcd.print(DHT.temperature);
+    lcd.setCursor(8, 0);
+    lcd.print("H=");
+    lcd.print(DHT.humidity);
+  
+    digitalWrite(11, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    lcd.setCursor(4, 1);
+    lcd.print("CAUTION");
+    noTone(BUZZER_PIN);
+  }
+  else if (DHT.temperature >= 32 && DHT.temperature <=35 &&  DHT.humidity >= 45) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("T=");
+    lcd.print(DHT.temperature);
+    lcd.setCursor(8, 0);
+    lcd.print("H=");
+    lcd.print(DHT.humidity);
+  
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    lcd.setCursor(0, 1);
+    lcd.print("EXTREME CAUTION");
+  }
+  else if (DHT.temperature >= 36 &&  DHT.temperature <=40 &&  DHT.humidity >= 45) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("T=");
+    lcd.print(DHT.temperature);
+    lcd.setCursor(8, 0);
+    lcd.print("H=");
+    lcd.print(DHT.humidity);
+  
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, LOW);
+    lcd.setCursor(4, 1);
+    lcd.print("DANGER");
+
+    tone(BUZZER_PIN, 523, 300);
+    delay(300);
+    noTone(BUZZER_PIN);
+    delay(100);
+  }
+  else if (DHT.temperature >= 41 &&  DHT.humidity >= 45) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("T=");
+    lcd.print(DHT.temperature);
+    lcd.setCursor(8, 0);
+    lcd.print("H=");
+    lcd.print(DHT.humidity);
+  
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, HIGH);
+    lcd.setCursor(1, 1);
+    lcd.print("EXTREME DANGER");
+
+    tone(BUZZER_PIN, 600, 500);
+    delay(300);
+    noTone(BUZZER_PIN);
+    delay(100);
+  }
+  else if (DHT.temperature <= 27) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("T=");
+    lcd.print(DHT.temperature);
+    lcd.setCursor(8, 0);
+    lcd.print("H=");
+    lcd.print(DHT.humidity);
+  
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    lcd.setCursor(2, 1);
+    lcd.print("NORMAL TEMP");
+  }
+  delay(1000);
+}
+
---
+++ incubator/PROJECT_INCUBATOR.ino
@@ -0,0 +1,60 @@
+#include<dht.h>
+dht DHT;
+#define DHT11_PIN 7
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#define buzzer 8 
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+ lcd.begin();
+ pinMode (11,OUTPUT);
+  pinMode (12,OUTPUT);
+  pinMode (buzzer,OUTPUT);
+
+}
+
+void loop() {
+
+  if (DHT.temperature >= 31 && DHT.humidity >=50){
+     int chk = DHT.read11(DHT11_PIN);
+     lcd.clear();
+  lcd.backlight();
+   lcd.setCursor(0, 0);
+ lcd.print("T=");
+ lcd.print(DHT.temperature);
+  lcd.setCursor(8, 0);
+  lcd.print("H=");
+ lcd.print(DHT.humidity);
+  delay (1000);
+    digitalWrite (12,HIGH);
+    digitalWrite (11,LOW);
+    lcd.setCursor(4, 1);
+    lcd.print ("WARNING!");
+  
+  tone(buzzer,523,300);
+    delay(300);
+    noTone(buzzer);
+    delay(100);
+    
+
+  
+}
+  else if (DHT.temperature <=27){
+     int chk = DHT.read11(DHT11_PIN);
+  lcd.backlight();
+   lcd.setCursor(0, 0);
+ lcd.print("T=");
+ lcd.print(DHT.temperature);
+  lcd.setCursor(8, 0);
+  lcd.print("H=");
+ lcd.print(DHT.humidity);
+  delay (1000);
+    digitalWrite (11,HIGH);
+    digitalWrite (12,LOW);
+    lcd.setCursor(3, 1);
+    lcd.print ("NORMAL TEMP");
+    noTone(buzzer);
+}
+}
